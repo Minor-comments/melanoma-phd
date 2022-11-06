f1eab9d08b2e8930540ab96205a561aa0a4fe61d
Marcel Gozalbo Baró <mgozalbo@opground.com>
2022-11-06 17:23:28 +0100

    FIX: Pass each Google Cloud private parameter as env variable.

--- melanoma_phd/config/AppConfig.py
+++ melanoma_phd/config/AppConfig.py
@@ -30,8 +30,21 @@ class AppConfig(JsonConfig):
 
     @property
     def google_service_account_info(self) -> Dict:
-        info = os.environ.get("GOOGLE_SERVICE_ACCOUNT_INFO", None)
-        if not info:
+        project_id = os.environ.get("GOOGLE_SERVICE_ACCOUNT_PROJECT_ID", None)
+        if project_id:
+            return {
+                "type": "service_account",
+                "project_id": project_id,
+                "private_key_id": os.environ["GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID"],
+                "private_key": os.environ["GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY"],
+                "client_email": os.environ["GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL"],
+                "client_id": os.environ["GOOGLE_SERVICE_ACCOUNT_CLIENT_ID"],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/melanoma-phd-client%40melanoma-phd.iam.gserviceaccount.com",
+            }
+        else:
             folder_name = os.path.dirname(os.path.abspath(__file__))
             service_account_file = os.path.join(folder_name, "service_account_file.json")
             if os.path.exists(service_account_file):
