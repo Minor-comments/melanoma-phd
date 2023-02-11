da649d4031238438efc9c0ea6089035cfab35841
Lluis <l.salord.quetglas@gmail.com>
2023-02-11 12:45:56 +0100

    REFACTOR: Remove unused Streamlit pages

--- streamlit_app/pages/2_blood_parameters.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from streamlit_app.AppLoader import AppLoader
-
-if __name__ == "__main__":
-    st.title("Blood Parameters Database")
-    with AppLoader() as app:
-        st.dataframe(app.database.blood_parameters_database.dataframe)
--- streamlit_app/pages/3_lf_dna_populations.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from streamlit_app.AppLoader import AppLoader
-
-if __name__ == "__main__":
-    st.title("LF_DNA Populations Database")
-    with AppLoader() as app:
-        st.dataframe(app.database.lf_dna_populations_database.dataframe)
